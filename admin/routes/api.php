bff9f685911a1a9af1a368954b1502f0cbb48308
root <gnatishen@gmail.com>
2018-09-03 12:53:21 +0300

    add api for frontend

--- admin/routes/api.php
+++ admin/routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('items', 'ItemController@index');
+Route::get('item/{id}', 'ItemController@show');
+Route::post('items', 'ItemController@store');
+Route::put('items/{id}', 'ItemController@update');
+Route::delete('items/{id}', 'ItemController@delete');
+
+//static pages 
+Route::get('pages', 'PageController@index');
+Route::get('page/{id}', 'PageController@show');
+Route::post('pages', 'PageController@store');
+Route::put('pages/{id}', 'PageController@update');
+Route::delete('pages/{id}', 'PageController@delete');
+
+//menu
+Route::get('menu', function()
+{
+	return menu('main', 'bootstrap');
+});
+
+//settings
+Route::get('settings', 'SettingController@index');
